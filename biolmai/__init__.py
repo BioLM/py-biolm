eede1b335de857525582dc721c71400f3a2185e0
Nikhil Haas <nikhil@nikhilhaas.com>
2023-09-07 08:55:21 -0700

    Bump version: 0.1.0 → 0.1.1

--- biolmai/__init__.py
+++ biolmai/__init__.py
@@ -1,7 +1,7 @@
 """Top-level package for BioLM AI."""
 __author__ = """Nikhil Haas"""
 __email__ = 'nikhil@biolm.ai'
-__version__ = '0.1.0'
+__version__ = '0.1.1'
 
 from biolmai.biolmai import get_api_token, api_call
 
