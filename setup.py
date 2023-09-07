6f7dc088f7670d72db4e051dc81de0075f7a86e3
Nikhil Haas <nikhil@nikhilhaas.com>
2023-09-07 09:22:47 -0700

    Add pandas to requirements

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open('README.rst') as readme_file:
 with open('HISTORY.rst') as history_file:
     history = history_file.read()
 
-requirements = ['Click>=6.0', 'requests', 'pandarallel<=1.6.5']
+requirements = ['Click>=6.0', 'requests', 'pandarallel<=1.6.5', 'pandas']
 
 test_requirements = ['pytest>=3', ]
 
