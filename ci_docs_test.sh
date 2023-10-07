d539be8c658715e147c20b47db99b34341a788cf
Nikhil Haas <nikhil@nikhilhaas.com>
2023-10-07 16:27:26 -0700

    Maybe fix fonts

--- ci_docs_test.sh
+++ ci_docs_test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -ev
 if [ "${TRAVIS_PYTHON_VERSION}" = "3.9" ]; then
-  sudo apt-get update && sudo apt-get install -y font-manager msttcorefont || true
+  sudo apt-get update && sudo apt-get install -y font-manager || true
   pip install -r requirements_docs.txt
   make docs
 fi
