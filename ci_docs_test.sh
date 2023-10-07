2342e54caed864a5a59654d55283d7250d8701b8
Nikhil Haas <nikhil@nikhilhaas.com>
2023-10-07 16:56:16 -0700

    Add docs build to tox to improve testing

--- ci_docs_test.sh
+++ ci_docs_test.sh
@@ -3,5 +3,5 @@ set -ev
 if [ "${TRAVIS_PYTHON_VERSION}" = "3.9" ]; then
   sudo apt-get update && sudo apt-get install -y font-manager || true
   pip install -r requirements_docs.txt
-  make docs
+  tox -e build_docs
 fi
