c7a5696b3794db40e9f1146bce1b9f8700dcbf1c
Nikhil Haas <nikhil@nikhilhaas.com>
2023-10-07 17:04:04 -0700

    Remove duplicate docs reqs install

--- ci_docs_test.sh
+++ ci_docs_test.sh
@@ -2,6 +2,5 @@
 set -ev
 if [ "${TRAVIS_PYTHON_VERSION}" = "3.9" ]; then
   sudo apt-get update && sudo apt-get install -y font-manager || true
-  pip install -r requirements_docs.txt
   tox -e build_docs
 fi
