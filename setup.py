fb050ea7293fa415c31975f4471ee271b45eb30c
Nikhil Haas <nikhil@nikhilhaas.com>
2023-12-26 11:51:32 -0800

    Put back aiodns in reqs since pure-python deps won't be possible

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open("README.rst") as readme_file:
 with open("HISTORY.rst") as history_file:
     history = history_file.read()
 
-requirements = ["Click>=6.0", "requests", "pandas", "aiohttp"]
+requirements = ["Click>=6.0", "requests", "pandas", "aiohttp", "aiodns"]
 
 test_requirements = [
     "pytest>=3",
@@ -48,6 +48,6 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/BioLM/py-biolm",
-    version='0.1.8',
+    version='0.1.7',
     zip_safe=False,
 )
