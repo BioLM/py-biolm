4f42ad8fc39e2740a3ffa111d8fc35a60dc138e1
Nikhil Haas <nikhil@nikhilhaas.com>
2025-04-21 03:10:10 -0700

    Drop 3.7 support until fix test bug

--- setup.py
+++ setup.py
@@ -30,13 +30,14 @@ test_requirements = [
 setup(
     author="BioLM",
     author_email="support@biolm.ai",
-    python_requires=">=3.6",
+    python_requires=">=3.7",
     classifiers=[
-        "Development Status :: 2 - Pre-Alpha",
+        "Development Status :: 3 - Alpha",
         "Intended Audience :: Developers",
         "License :: OSI Approved :: Apache Software License",
         "Natural Language :: English",
         "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
