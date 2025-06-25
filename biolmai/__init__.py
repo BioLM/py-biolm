4d2e7ad32732ed21a19eca248473b2cb45434ada
Nikhil Haas <nikhil@nikhilhaas.com>
2025-06-24 21:56:30 -0700

    Bump version: 0.2.3 → 0.2.4

--- biolmai/__init__.py
+++ biolmai/__init__.py
@@ -1,7 +1,7 @@
 """Top-level package for BioLM AI."""
 __author__ = """Nikhil Haas"""
 __email__ = "nikhil@biolm.ai"
-__version__ = '0.2.3'
+__version__ = '0.2.4'
 
 from biolmai.cls import *
 from biolmai.client import BioLMApi, BioLMApiClient
