bf1377c88e41280760fe85de1befc071c069eaf3
Nikhil Haas <nikhil@nikhilhaas.com>
2025-11-10 20:19:17 -0800

    Bump version: 0.2.5 → 0.2.6

--- setup.py
+++ setup.py
@@ -62,6 +62,6 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/BioLM/py-biolm",
-    version='0.2.5',
+    version='0.2.6',
     zip_safe=False,
 )
