1981d50098e0731291cad9d355d00f262049a595
Nikhil Haas <nikhil@nikhilhaas.com>
2026-02-26 17:00:02 -0800

    Bump version: 0.3.0 → 0.4.0

--- docs/conf.py
+++ docs/conf.py
@@ -71,7 +71,7 @@ author = "Nikhil Haas"
 # the built documents.
 #
 # The short X.Y version.
-version = '0.3.0'
+version = '0.4.0'
 # The full version, including alpha/beta/rc tags.
 release = version
 
